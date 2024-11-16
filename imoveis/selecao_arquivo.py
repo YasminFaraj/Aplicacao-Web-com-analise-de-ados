7931bd9e5c1ce052baf8c51b571266bbf3aba139
laura7164 <164506063+laura7164@users.noreply.github.com>
2024-11-16 17:50:26 -0300

    Add files via upload

--- imoveis/selecao_arquivo.py
+++ imoveis/selecao_arquivo.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, session
+from flask import Flask, request, render_template, redirect, url_for, session, flash
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
@@ -8,14 +8,14 @@ import tempfile
 import os
 
 app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Limita a 16MB o tamamho do arquivo
-app.secret_key = "your_secret_key"  # Necessário para usar sessões
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # limita a 16MB o tamamho do arquivo
+app.secret_key = "your_secret_key"  # necessário para usar sessões
 
-# Página inicial para upload do arquivo CSV
+# página inicial para upload do arquivo CSV
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        session.clear()  # Limpa qualquer dado armazenado na sessão
+        session.clear()  # limpa qualquer dado armazenado na sessão
         if 'arquivo' not in request.files:
             return "Nenhum arquivo foi enviado"
 
@@ -24,34 +24,34 @@ def index():
         if arquivo.filename == '':
             return "Escolha um arquivo válido"
 
-        # Lê o arquivo CSV
+        # lê o arquivo CSV
         data = pd.read_csv(arquivo)
 
-        # O arquivo estava grande, então essa parte cria um arquivo temporário para armazenas o csv
+        # o arquivo estava grande, então essa parte cria um arquivo temporário para armazenas o csv
         with tempfile.NamedTemporaryFile(delete=False, suffix='.csv') as arq_temp:
             data.to_csv(arq_temp.name, index=False)
-            session['arquivo_temporario'] = arq_temp.name  # Armazena o caminho do arquivo temporário na sessão
+            session['arquivo_temporario'] = arq_temp.name  # armazena o caminho do arquivo temporário na sessão
 
-        # Redireciona para a página de seleção de colunas, passando as colunas do CSV
+        # redireciona para a página de seleção de colunas, passando as colunas do CSV
         return redirect(url_for('selecao_colunas'))
 
     return render_template('index.html')
 
 
-# Página para seleção de colunas
+# página para seleção de colunas
 @app.route('/selecao_colunas', methods=['GET', 'POST'])
 def selecao_colunas():
-    # Carrega o dataframe da sessão (os dados temporários)
+    # carrega o dataframe da sessão (os dados temporários)
     arq_temp_caminho = session.get('arquivo_temporario')
     if arq_temp_caminho is None:
         return redirect(url_for('index'))
 
-    # Lendo o arquivo e reconhecendo cada coluna
+    # lendo o arquivo e reconhecendo cada coluna
     data = pd.read_csv(arq_temp_caminho)
     colunas = data.columns
 
     if request.method == 'POST':
-        # Salva as colunas escolhidas pelo usuário ou None se "Nenhuma" for selecionada
+        # salva as colunas escolhidas pelo usuário ou None se "Nenhuma" for selecionada
         session['tipo_col'] = request.form['tipo'] if request.form['tipo'] else None
         session['cidade_col'] = request.form['cidade'] if request.form['cidade'] else None
         session['bairro_col'] = request.form['bairro'] if request.form['bairro'] else None
@@ -62,51 +62,53 @@ def selecao_colunas():
         session['latitude_col'] = request.form['latitude'] if request.form['latitude'] else None
         session['longitude_col'] = request.form['longitude'] if request.form['longitude'] else None
 
-        # Redireciona para a página de configuração do modelo
+        # recupera as colunas selecionadas e filtra apenas as válidas
+        colunas_selecionadas = {
+            "bairro": session.get('bairro_col'),
+            "metragem": session.get('metragem_col'),
+            "quartos": session.get('quartos_col'),
+            "banheiros": session.get('banheiros_col'),
+            "tipo": session.get('tipo_col'),
+            "cidade": session.get('cidade_col'),
+            "latitude": session.get('latitude_col'),
+            "longitude": session.get('longitude_col'),
+            "preco": session.get('preco_col')
+        }
+        
+        # filtra apenas colunas válidas (que não são None)
+        colunas_validas = [coluna for coluna in colunas_selecionadas.values() if coluna is not None]
+        
+        # garante que pelo menos uma coluna foi selecionada
+        if not colunas_validas:
+            flash("Erro: Nenhuma coluna válida foi selecionada.", "error")
+            return redirect('/selecao_colunas')
+
+        # separa X e y do dataset
+        X = data[colunas_validas]
+        if session.get('preco_col') is None:
+            flash("Erro: A coluna de preço é obrigatória para treinar o modelo.", "error")
+            return redirect('/selecao_colunas')
+        
+        y = data[session['preco_col']]
+
+        # redireciona para a página de configuração do modelo
         return redirect(url_for('configura_modelo'))
 
     return render_template('selecao_colunas.html', columns=colunas)
 
 
-
-# Página para configuração do modelo e treinamento
+# página para configuração do modelo e treinamento
 @app.route('/configura_modelo', methods=['GET', 'POST'])
 def configura_modelo():
-    # Carrega os dados temporários de novo
+    # carrega os dados temporários de novo
     arq_temp_caminho = session.get('arquivo_temporario')
     if arq_temp_caminho is None:
         return redirect(url_for('index'))
 
     data = pd.read_csv(arq_temp_caminho)
 
-    # Recupera as colunas selecionadas e filtra apenas as válidas
-    colunas_selecionadas = {
-        "bairro": session.get('bairro_col'),
-        "metragem": session.get('metragem_col'),
-        "quartos": session.get('quartos_col'),
-        "banheiros": session.get('banheiros_col'),
-        "tipo": session.get('tipo_col'),
-        "cidade": session.get('cidade_col'),
-        "latitude": session.get('latitude_col'),
-        "longitude": session.get('longitude_col'),
-        "preco": session.get('preco_col')
-    }
-    
-    # Filtra apenas colunas válidas (que não são None)
-    colunas_validas = [coluna for coluna in colunas_selecionadas.values() if coluna is not None]
-    
-    # Garante que pelo menos uma coluna foi selecionada
-    if not colunas_validas:
-        return "Erro: Nenhuma coluna válida foi selecionada.", 400
-
-    # Separa X e y do dataset
-    X = data[colunas_validas]
-    if session.get('preco_col') is None:
-        return "Erro: A coluna de preço é obrigatória para treinar o modelo.", 400
-    y = data[session['preco_col']]
-
     if request.method == 'POST':
-        # Configura o modelo com base nas escolhas do usuário
+        # configura o modelo com base nas escolhas do usuário
         modelo_escolhido = request.form.get("modelo")
         if modelo_escolhido == 'RandomForest':
             n_estimators = int(request.form.get("n_estimators", 100))
@@ -118,20 +120,19 @@ def configura_modelo():
         else:
             return "Modelo não suportado"
 
-        # Divide os dados em treino e teste
+        # divide os dados em treino e teste
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-        # Treina o modelo
+        # treina o modelo
         model.fit(X_train, y_train)
 
-        # Faz predições e calcula a acurácia
+        # faz predições e calcula a acurácia
         y_pred = model.predict(X_test)
         acuracia = accuracy_score(y_test, y_pred)
 
         return f"Modelo treinado com acurácia: {acuracia}"
 
     return render_template('configura_modelo.html')
-
 
 
 if __name__ == '__main__':
